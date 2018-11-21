2dff58c845f78bfa27f7dc9d1e0cd34378f24ee7
Satoshi <st39mart@gmail.com>
2018-11-21 15:01:22 +0900

    increment action add

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCRMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
