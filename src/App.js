ef93e24b6040af15ce5c26ff0069147161099efd
Satoshi <st39mart@gmail.com>
2018-11-21 12:18:32 +0900

    create class components and function componenti

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render(){
-    return(
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => { console.log("Iam clicked!") }} />
-    </React.Fragment>
-   )
- }
+// class App extends Component {
+//   render(){
+//     return(
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => { console.log("Iam clicked!") }} />
+//     </React.Fragment>
+//    )
+//  }
+// }
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+const Cat = () => {
+  return <div>meo</div>
+}
+
+
 export default App;
