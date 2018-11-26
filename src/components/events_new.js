847941bfed4e0e4a8a38813e29493db77ae5454f
Satoshi <st39mart@gmail.com>
2018-11-26 15:52:45 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -29,7 +29,7 @@ class EventsNew extends Component {
   }
 
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -39,7 +39,7 @@ class EventsNew extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={false} />
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
           <Link to="/">Cancel</Link>
         </div>
      </form>
